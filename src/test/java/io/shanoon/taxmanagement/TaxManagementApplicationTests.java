146938dc3b2d385549cf7c235eef3b8dd9928036
shanoonissaka <shanoon.issaka48@gmail.com>
2023-08-15 15:31:03 +0000

    Task: get all spring dependencies

---
+++ src/test/java/io/shanoon/taxmanagement/TaxManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shanoon.taxmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
